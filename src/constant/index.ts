37acb750d1660090acadae24784daeeda5c573d7
Yasin Elbüz <yasinelbuz@gmail.com>
2023-08-27 03:13:15 +0300

    created constant files

---
+++ src/constant/index.ts
@@ -0,0 +1,5 @@
+import routes from "./router";
+import theme from "./theme";
+import globalStyles from "./globalStyles";
+
+export default { routes, theme, globalStyles };
---
+++ src/constant/router.tsx
@@ -0,0 +1,31 @@
+import Component from "@/components";
+import Pages from "@/pages";
+
+const routes = [
+	{
+		path: "/",
+		element: (
+			<Component.Layout>
+				<Pages.Home />
+			</Component.Layout>
+		),
+	},
+	{
+		path: "movie-detail",
+		element: (
+			<Component.Layout>
+				<Pages.MovieDetail />
+			</Component.Layout>
+		),
+	},
+	{
+		path: "*",
+		element: (
+			<Component.Layout>
+				<Pages.PageNotFound />
+			</Component.Layout>
+		),
+	},
+];
+
+export default routes;
