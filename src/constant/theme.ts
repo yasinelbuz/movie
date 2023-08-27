96adb57b574dbee429284d490a407bc5a48dab33
Yasin Elbüz <yasinelbuz@gmail.com>
2023-08-27 03:12:31 +0300

    added theme and globalstyles

---
+++ src/constant/theme.ts
@@ -0,0 +1,22 @@
+const color = {
+	primaryColor: "#1450A3",
+	secondaryColor: "#337CCF",
+	primaryTextColor: "#000",
+	secondaryTextColor: "#fff",
+};
+
+const fonts = {
+	primaryFont: "Roboto",
+};
+
+const fontSize = {
+	sm: "0.8rem",
+	base: "1rem",
+	xl: "1.25rem",
+	"2xl": "1.563rem",
+	"3xl": "1.953rem",
+	"4xl": "2.441rem",
+	"5xl": "3.052rem",
+};
+
+export default { color, fonts, fontSize };
